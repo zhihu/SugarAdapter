2d3a20d8bd7e8bfc668ea850734b41a8e76c31f3
Matthew Lee <matthewlee0725@gmail.com>
2018-12-13 18:13:37 +0800

    @Layout Inherited

--- sugaradapter-annotation/src/main/java/com/zhihu/android/sugaradapter/Layout.java
+++ sugaradapter-annotation/src/main/java/com/zhihu/android/sugaradapter/Layout.java
@@ -19,12 +19,14 @@ package com.zhihu.android.sugaradapter;
 import android.support.annotation.LayoutRes;
 
 import java.lang.annotation.ElementType;
+import java.lang.annotation.Inherited;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
 @Retention(RetentionPolicy.CLASS)
 @Target(ElementType.TYPE)
+@Inherited
 public @interface Layout {
     @LayoutRes
     int value() default 0;
